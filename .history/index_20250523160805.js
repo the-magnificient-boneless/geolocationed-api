ee863a4e0ac3fb7ab62daaf65e65422240112ec0
Greg Ayvar <themagnificientboneless@gmail.com>
2025-05-23 16:08:46 -0600

    server express added

---
+++ .history/index_20250523160805.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import fetch from 'node-fetch';
+
+
+app.use(express.json());
+app.use(require("cors")());
+
+app.get("/", (req, res) => {
+  res.send("API está corriendo 🚀");
+});
+
+app.post("/api/save-location", (req, res) => {
+  console.log("BODY:", req.body);
+  res.json({ status: "ok", location: req.body });
+});
+
+const PORT = process.env.PORT || 3000; // 👈 NECESARIO
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
